d3789dd5e70197c997257e9c267ee580f2998954
117CarlosCoder <carloslopez202031871@cunoc.edu.gt>
2022-05-26 11:20:07 -0600

    Inicio

---
+++ segundo-proyecto/src/main/java/com/proyecto2/inicial/App.java
@@ -0,0 +1,13 @@
+package com.proyecto2.inicial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
